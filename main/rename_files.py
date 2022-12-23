0c7bfb44452f665fa519d7ee956ea9fff12871f3
peterlohaus <33315207+peterlohaus@users.noreply.github.com>
2022-12-23 19:24:26 +0100

    Refactor

--- main/rename_files.py
+++ main/rename_files.py
@@ -15,19 +15,25 @@ def renameFiles():
         kind = filetype.guess(filePath)
         newFileName = fileName
         if kind.extension in settings.SUPPORTED_IMAGE_EXTENSIONS:
-            image = Image.open(filePath)
-            exif = image.getexif()
-            image.close()
-            timestamp = datetime.strptime(exif.get(306), '%Y:%m:%d %H:%M:%S')
-            make = exif.get(271)
-            model = exif.get(272)
-            newFileName = timestamp.strftime(settings.TIMESTAMP_FORMAT_CODE) + '_' + make.replace(
-                ' ', '_') + '_' + model.replace(' ', '_') + '.' + kind.extension
+            newFileName = getNewImageFilename(filePath, kind.extension)
         elif kind.extension in settings.SUPPORTED_VIDEO_EXTENSIONS:
-            newFileName = uuid.uuid4().hex + '.' + kind.extension
+            newFileName = getNewVideoFileName(kind.extension)
         else:
             return
         try:
             os.rename(filePath, basePath + newFileName)
         except:
             pass
+
+def getNewVideoFileName(fileExtension):
+    return uuid.uuid4().hex + '.' + fileExtension
+
+def getNewImageFilename(filePath, fileExtension):
+    image = Image.open(filePath)
+    exif = image.getexif()
+    image.close()
+    timestamp = datetime.strptime(exif.get(306), '%Y:%m:%d %H:%M:%S')
+    make = exif.get(271)
+    model = exif.get(272)
+    return timestamp.strftime(settings.TIMESTAMP_FORMAT_CODE) + '_' + make.replace(
+        ' ', '_') + '_' + model.replace(' ', '_') + '.' + fileExtension
